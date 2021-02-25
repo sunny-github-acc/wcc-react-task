442f9dbc319d128c33ee3b1e347441d69c0f5b79
sunny <karolis.kazak@gmail.com>
2021-02-25 17:57:37 +0200

    Updated SearchTab component and styles

--- src/components/SearchTab.js
+++ src/components/SearchTab.js
@@ -1,15 +1,41 @@
+import { useState } from 'react'
 import {ReactComponent as SearchLogo} from '../icons/search.svg';
 import {ReactComponent as MovieLogo} from '../icons/movie.svg';
 
-const SearchTab = ({ title, onAdd, showAdd }) => {
+const SearchTab = () => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    e.preventDefault();
+    setIsInputFocused(true);
+  }
+
+  const handleBlur = () => {
+    setIsInputFocused(false);
+  }
+
+  let inputState = isInputFocused ? "passive" : "active";
+  let searchDisplay = isInputFocused ? "no-display" : "";
+
   return (
     <form action="#">
-      <label for="search" className="label-input-text">
-        <MovieLogo className="movie logo" />
+      <label htmlFor="search" className="label-input-text">
+        <MovieLogo className="movie" />
+        <span className={`placeholder ${inputState}`}>
+          <MovieLogo className="movie" />
+          <span className="text">Enter movie name</span>
+        </span>
       </label>
-      <input type="text" id="search" name="search" className="input-text" placeholder="Enter movie name" />
-      <label for="submit" className="label-submit">
-        <SearchLogo className="search logo" />
+      <input type="text" id="search" name="search" className={`input-text ${inputState}`}
+        onFocus={(e) => handleFocus(e)}
+        onBlur={() => handleBlur()} />
+      <div className={`container movies ${inputState}`}>
+        <h3>Office</h3>
+        <h4>9.0 Rating, 2007</h4>
+      </div>
+
+      <label htmlFor="submit" className={`label-submit ${searchDisplay}`}>
+        <SearchLogo className="search" />
       </label>
       <input id="submit" type="submit" value="" hidden />
     </form>
