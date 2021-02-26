b186c6f8679c767f78125ac52420cfbe94a4bdc0
sunny <karolis.kazak@gmail.com>
2021-02-26 21:34:26 +0200

    Updated SearchTab component and styles

--- src/components/SearchTab.js
+++ src/components/SearchTab.js
@@ -1,41 +1,56 @@
 import { useState } from 'react'
-import {ReactComponent as SearchLogo} from '../icons/search.svg';
-import {ReactComponent as MovieLogo} from '../icons/movie.svg';
+import { ReactComponent as SearchLogo } from '../icons/search.svg'
+import { ReactComponent as MovieLogo } from '../icons/movie.svg'
+import Movies from "./Movies"
 
 const SearchTab = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
+  const [inputStateValue, setInputValue] = useState("");
+  const [placeholderStateValue, setPlaceholderValue] = useState("Enter a movie name");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
 
   const handleFocus = (e) => {
     e.preventDefault();
     setIsInputFocused(true);
+    setPlaceholderValue("Enter a movie name");
   }
 
   const handleBlur = () => {
     setIsInputFocused(false);
+    inputStateValue ? 
+      setPlaceholderValue(inputStateValue) : 
+      setPlaceholderValue("Enter a movie name");
   }
 
-  let inputState = isInputFocused ? "passive" : "active";
+  let inputState = isInputFocused ? "focused" : "not-focused";
   let searchDisplay = isInputFocused ? "no-display" : "";
+  let placeholderValue = placeholderStateValue;
 
   return (
     <form action="#">
       <label htmlFor="search" className="label-input-text">
-        <MovieLogo className="movie" />
-        <span className={`placeholder ${inputState}`}>
-          <MovieLogo className="movie" />
-          <span className="text">Enter movie name</span>
-        </span>
+        <MovieLogo className="movie-logo" />
+        <span className={`placeholder ${inputState}`}>{placeholderValue}</span>
       </label>
-      <input type="text" id="search" name="search" className={`input-text ${inputState}`}
-        onFocus={(e) => handleFocus(e)}
-        onBlur={() => handleBlur()} />
-      <div className={`container movies ${inputState}`}>
-        <h3>Office</h3>
-        <h4>9.0 Rating, 2007</h4>
+      <div className={`container-input-movies ${inputState}`}>
+        <div className={`container-input-text ${inputState}`}>
+          <MovieLogo className={`movie-logo ${inputState}`} />
+          <input type="text" id="search" name="search"
+            className={`input-text ${inputState}`}
+            onChange={(e) => handleChange(e)}
+            onFocus={(e) => handleFocus(e)}
+            onBlur={() => handleBlur()} />
+        </div>
+        <Movies 
+          className={`movie-titles ${inputState}`}
+          search={inputStateValue} />
       </div>
 
       <label htmlFor="submit" className={`label-submit ${searchDisplay}`}>
-        <SearchLogo className="search" />
+        <SearchLogo className="search-logo" />
       </label>
       <input id="submit" type="submit" value="" hidden />
     </form>
